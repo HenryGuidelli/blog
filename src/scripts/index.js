742e5c874e09db429ff885276292fe650c4f238e
henryguidelli <contato.henryguidelli@gmail.com>
2023-06-22 22:22:37 -0300

    teste

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,20 @@
-var link_json = "https://raw.githubusercontent.com/HenryGuidelli/blog/main/src/json/teste.json?token=GHSAT0AAAAAACEG57HVLBTTRBXVC4HOQ36YZEU6DVA";
+var link_json = "https://raw.githubusercontent.com/HenryGuidelli/blog/main/src/json/teste.json?token=GHSAT0AAAAAACEG57HUMLBUX5AUP7EUJ5JAZEU6P4Q";
 
 fetch(link_json)
   .then(response => response.json())
   .then(data => {
 
-    post = data.post;
+    post = data;
 
-    for (key in data) {
-        console.log(data);
-    }
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          const item = data.post0000.nome;
+        //   console.log("Chave:", key);
+          console.log("Valor:", item);
+        }
+      } 
 
-    console.log(data.post0000);
+    // console.log(data.post0000);
   })
   .catch(error => {
     console.error('Ocorreu um erro:', error);
