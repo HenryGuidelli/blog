0c8fe401ba2e36ff2850ef35d97fd86bafef5501
henryguidelli <contato.henryguidelli@gmail.com>
2023-06-22 22:09:55 -0300

    again

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,17 @@
-var link_json = "https://raw.githubusercontent.com/HenryGuidelli/blog/main/src/json/teste.json?token=GHSAT0AAAAAACEG57HVN5NZ4QN37XJLAHAYZEU54IQ";
+var link_json = "https://raw.githubusercontent.com/HenryGuidelli/blog/main/src/json/teste.json?token=GHSAT0AAAAAACEG57HVLBTTRBXVC4HOQ36YZEU6DVA";
 
 fetch(link_json)
   .then(response => response.json())
   .then(data => {
-    // Aqui você pode acessar os dados do arquivo JSON
+
+    post = data.post;
+
+    for (key in data) {
+        console.log(data);
+    }
+
     console.log(data.post0000);
   })
   .catch(error => {
-    // Em caso de erro na requisição
     console.error('Ocorreu um erro:', error);
   });
